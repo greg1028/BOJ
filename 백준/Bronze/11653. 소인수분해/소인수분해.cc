b3cadb94db5a5877728fe271ffd2892d62cb2cf6
Duk Hyun Yoo <83526669+greg1028@users.noreply.github.com>
2022-08-19 15:37:37 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N == 1) return 0;
+
+    for (int i = 2; i <= N; i++) {
+        while (N % i == 0) {
+            cout << i << endl;
+            N /= i;
+        }
+    }
+
+    return 0;
+}
