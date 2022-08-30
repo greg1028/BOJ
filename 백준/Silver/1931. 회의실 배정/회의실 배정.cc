ceee946d4738e399f6a4a57a787f0127053b8dc7
Duk Hyun Yoo <83526669+greg1028@users.noreply.github.com>
2022-08-30 17:51:01 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<pair<int, int>> meeting;
+
+    int N, srt, end, cnt = 0;
+    cin >> N;
+
+    for (int i = 0; i < N; i++){
+        cin >> srt >> end;
+        meeting.push_back(make_pair(end, srt));
+    }
+    sort(meeting.begin(), meeting.end());
+
+    srt = 0, end = 0;
+
+    for (int i = 0; i < N; i++){
+        if (end <= meeting[i].second){
+            srt = meeting[i].second;
+            end = meeting[i].first;
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
