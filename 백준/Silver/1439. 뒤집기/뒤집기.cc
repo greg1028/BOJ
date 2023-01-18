1753d1bf28e890090451ce4cdf165cfe24d492ad
Duk Hyun Yoo <greg1028@naver.com>
2023-01-18 16:13:25 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
+string S;
+int cnt = 1;
 
 int main(){
-    string S;
-    int cnt = 1;
     cin >> S;
-    
-    for (int i = 0; i < S.length() - 1; i++){
-        if (S[i] != S[i + 1]) cnt++; 
-    }
+
+    for (int i = 0; i < S.length() - 1; i++) if (S[i] != S[i + 1]) cnt++;
     
     cout << cnt/2;
-    
     return 0;
 }
